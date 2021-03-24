9ad9efbc11a311e75bb58f4b6d89b10c0abb4894
LazaroMPerez <lazpz61@gmail.com>
2021-03-24 00:02:10 -0400

    form submit attempt1

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import moment from "moment";
+
+import FeatureContainer from "./features/containers";
+
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Features Practice</h1>
+        <div>Todays date is {moment().format("MMMM Do YYYY")}</div>
+        
+        <h1>This will be where my list for quesitons and the inputs will be</h1>
       </div>
     );
   }
---
+++ src/components/button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function(props) {
+    return(
+        <button classNAme="my-button">Click Here</button>
+    )
+}
